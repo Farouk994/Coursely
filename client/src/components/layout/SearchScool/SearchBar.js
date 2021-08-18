c92a96f66ab14d3f5f273c44e4312c7fc9e33982
Farouk <faroukkisuule55@gmail.com>
2021-08-18 12:41:25 -0400

    Search schhol by id functionality not yet completed but functionality works for only getting one school and also to access all schools

---
+++ client/src/components/layout/SearchScool/SearchBar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Fragment, useState } from "react";
+import axios from "axios";
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log("Password does not Match");
+    } else {
+      const newUser = {
+        name,
+        email,
+        password,
+      };
+      try {
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+        const body = JSON.stringify(newUser);
+        const res = await axios.post("api/users", body, config);
+        console.log(res.data);
+      } catch (err) {
+        console.log(err.response.data);
+      }
+    }
+  };
+  return (
+    <Fragment>
+    
+    </Fragment>
+  );
+};
+
+export default Register;
