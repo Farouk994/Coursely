733e12a6c2e18cf2bbac38ee4e9676efa49dbb86
Farouk <faroukkisuule55@gmail.com>
2021-08-18 08:13:08 -0400

    Added more components like getting school id and also initialised the routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,19 @@ import "./App.css";
 import Navbar from "./components/layout/Navbar/Navbar";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Landing from "./components/layout/Landing/Landing";
+import Schools from "./components/layout/School/Schools";
+import GetSchool from "./components/layout/School/SchoolById";
+// import SchoolReport from "./components/layout/School/SchoolReport";
 
 function App() {
    return (
-      <div>
-         <Router>
-            <Navbar />
-            <Route exact to="/" component={Landing} />
-         </Router>
-      </div>
+      <Router>
+         <Navbar />
+         <Route exact path="/" component={Landing} />
+         <Route exact path="/school" component={Schools} />
+         <Route exact path="/getschool" component={GetSchool} />
+         {/* <Route exact path="/schoolreport" component={SchoolReport} /> */}
+      </Router>
    );
 }
 
